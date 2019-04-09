d0575710bb192e7e36c8178c304fad3ac1fca7a1
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-04-08 21:19:09 -0700

    working on match logic

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'shooting',
+  password: '',
   database: 'friend_finder_db'
 });
 
@@ -54,6 +54,56 @@ app.post('/api/scores-insert/:friend_id', function(req, res){
 	  });
 	});
 });
+
+app.get('/match', function(req, res) {
+  connection.query('SELECT friends.name, friends.picture_link, matched.otherId FROM friends LEFT JOIN (SELECT them otherId, SUM(diff)-COUNT(*) score FROM (SELECT  their.friend_id them, ABS(their.score-my.score) diff FROM scores their LEFT JOIN scores my ON their.question_id=my.question_id WHERE their.friend_id!=my.friend_id AND my.friend_id=72) t1 GROUP BY them ORDER BY score)matched ON friends.id = matched.otherId WHERE friends.id = matched.otherId LIMIT 1', function(error, results, fields) {
+    if (error) res.send(error)
+    else res.json(results);
+  });
+});
+
+
+// select them otherId, sum(diff)-count(*) score from
+// (
+// select  their.friend_id them, ABS(their.score-my.score) diff
+// from scores their, scores my 
+// where their.friend_id!=my.friend_id 
+// and my.friend_id=72
+// and their.question_id=my.question_id
+// ) t1
+// group by them
+// order by score;
+
+// select them otherId, sum(diff)-count(*) score from
+// (
+// select  their.friend_id them, ABS(their.score-my.score) diff
+// from scores their 
+// left join scores my 
+// on their.question_id=my.question_id
+// where their.friend_id!=my.friend_id 
+// and my.friend_id=72
+// ) t1
+// group by them
+// order by score;
+
+// select friends.name, friends.picture_link, matched.otherId
+// from friends
+// left join
+//  (
+// select them otherId, sum(diff)-count(*) score from
+// (
+// select  their.friend_id them, ABS(their.score-my.score) diff
+// from scores their, scores my 
+// where their.friend_id!=my.friend_id 
+// and my.friend_id=72
+// and their.question_id=my.question_id
+// ) t1
+// group by them
+// order by score
+// )matched
+// on friends.id = matched.otherId
+// WHERE friends.id = matched.otherId
+// LIMIT 1;
 
 app.listen(3001, function() {
   console.log('listening on 3001');
